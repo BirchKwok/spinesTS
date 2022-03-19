a2b3ddcd8aedebee259baed98abb67e6837ad8c8
BirchKwok <BirchKwok@gmail.com>
2022-03-19 23:32:44 +0800

    Change the plot2d function to optimize the plot layout

--- spinesTS/base/__init__.py
+++ spinesTS/base/__init__.py
@@ -1,3 +1,3 @@
 from ._base_mixin import EstimatorMixin
 from ._const import DataTS
-from ._torch_mixin import TorchModelMixin
+from ._torch_mixin import TorchModelMixin, device
--- spinesTS/plotting/_plot_base.py
+++ spinesTS/plotting/_plot_base.py
@@ -1,62 +1,54 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def plot1d(*s, figsize=(12, 8)):
     """Plot series"""
     plt.figure(figsize=figsize)
     for i in range(len(s)):
-        plt.plot(s[i], label='label_'+str(i+1))
+        plt.plot(s[i], label='label_' + str(i + 1))
         plt.legend()
 
     plt.show()
 
 
-def plot2d(*s, labels=None, fig_num_or_slice=30, figsize=None, legend=True):
+def plot2d(*S, x=None, labels=None, eval_slices='[:30]',
+           figsize=(10, 6), legend=True, subplots_shape=(-1, 5)):
     """
     plot multi steps.
-    fig_num: Number of pictures drawn, fig_num = -1 drawn all pictures
     """
-    assert np.sum([i.ndim == 2 for i in s]) == len(s)
-    assert isinstance(fig_num_or_slice, int) or isinstance(fig_num_or_slice, slice)
+    assert np.sum([i.ndim == 2 for i in S]) == len(S)
+    assert isinstance(eval_slices, str)
 
-    if isinstance(fig_num_or_slice, int):
-        max_row = np.max([i.shape[0] for i in s])
-        if fig_num_or_slice != -1:
-            num = fig_num_or_slice if max_row > fig_num_or_slice else max_row
-        else:
-            num = max_row
-        _ = list(s)
-    else:
-        _ = []
-        for i in range(len(s)):
-            _.append(s[i][fig_num_or_slice])
+    fig = plt.figure(facecolor='w', figsize=figsize)
 
-        max_row = np.max([i.shape[0] for i in _])
-        num = max_row
+    _to_plot_array = []
+    for _s in range(len(S)):
+        _v = eval('S[_s]' + eval_slices)
+        if not isinstance(_v[0], (list, tuple, np.ndarray, pd.Series)):
+            _v = [_v]
+        _to_plot_array.append(_v)
 
-    cols = 5 if num >= 5 else num
-    rows = num // 5 if num % 5 == 0 else num // 5 + 1
+    num = max([len(_) for _ in _to_plot_array])
 
-    width = 20
-    high = rows / cols * 6 if 1 <= rows < 3 else rows * 2.5
+    cols = subplots_shape[-1] if num >= 5 else 1
+    rows = num if num < 5 else (num // subplots_shape[-1] + 1 if num % subplots_shape[-1] > 0
+                                else num // subplots_shape[-1])
 
-    fig_size = figsize if figsize is not None else (width, high)
-    plt.figure(figsize=fig_size)
-
-    for j in range(len(_)):
-        fig_index = 0
+    for j in range(len(_to_plot_array)):
         for i in range(1, num + 1):
-            if len(_[j]) > fig_index:
-                plt.subplot(rows, cols, i)
-                if labels is None:
-                    label = f'label_' + str(j + 1)
-                else:
-                    label = labels[j]
-                plt.plot(_[j][fig_index], label=label)
-                if legend:
-                    plt.legend()
-            fig_index += 1
-            if fig_index > num:
-                break
-    plt.show()
+            ax = plt.subplot(rows, cols, i)
+            if labels is None:
+                label = f'label_' + str(j + 1)
+            else:
+                label = labels[j]
+            if x is not None:
+                ax.plot(x, _to_plot_array[j][i-1], label=label, ls='-')
+            else:
+                ax.plot(_to_plot_array[j][i-1], label=label, ls='-')
+            if legend:
+                ax.legend()
+
+    fig.tight_layout()
+    return fig
