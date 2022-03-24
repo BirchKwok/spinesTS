80111fbd1d9587d842fee0f937476bcd8f612b46
BirchKwok <BirchKwok@gmail.com>
2022-03-24 22:58:44 +0800

    renamed file

--- spinesTS/utils/_torch_ops.py
+++ spinesTS/utils/_torch_ops.py
@@ -22,7 +22,7 @@ def one_dim_tensor_del_elements(tensor, start_index, end_index=None):
         elif start_index == -1 or start_index == len(tensor):
             return tensor[:-1]
         else:
-            return torch.concat((tensor[:index], tensor[index+1:]))
+            return torch.concat((tensor[:start_index], tensor[start_index+1:]))
     else:
         assert end_index >= start_index
         return torch.concat((tensor[:start_index], tensor[end_index+1:]))
