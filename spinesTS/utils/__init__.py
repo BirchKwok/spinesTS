29059be07de8bf0e92438bf293e503a47e3dc0ae
birchmyname <40603201+BirchKwok@users.noreply.github.com>
2022-03-24 17:50:04 +0800

    Update __init__.py
    Added one_dim_tensor_del_elements function

--- spinesTS/utils/__init__.py
+++ spinesTS/utils/__init__.py
@@ -2,3 +2,4 @@ from ._validation import check_x_y
 from ._metaestimator import available_if, func_has_params
 from ._set_seed import seed_everything
 from ._torch_summary import torch_summary
+from ._torch_ops import one_dim_tensor_del_elements
