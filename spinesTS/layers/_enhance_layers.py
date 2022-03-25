45723c7d05bd87f44146bcca77a0e212dffd3e6a
birchmyname <40603201+BirchKwok@users.noreply.github.com>
2022-03-25 09:51:39 +0800

    added parameter of scaling of gaussian noise

--- spinesTS/layers/_enhance_layers.py
+++ spinesTS/layers/_enhance_layers.py
@@ -5,17 +5,23 @@ from torch import nn
 class GaussianNoise1d(nn.Module):
     """Adds Gaussian noise to the input tensor.
     
+    Parameters
+    ----------
+    level: float, the scaling multiple of gaussian noise
+    device: str or None, device name
+    
     Returns
     -------
     torch.Tensor
     """
 
-    def __init__(self, device=None):
+    def __init__(self, level=0.1, device=None):
         super(GaussianNoise1d, self).__init__()
         self.device = device
+        self.level = level
 
     def forward(self, x):
-        return x + torch.mul(torch.randn_like(x, device=self.device), 1 / torch.median(x) / 60)
+        return x + torch.mul(torch.randn_like(x, device=self.device), 1 / torch.median(x) * self.level)
 
 
 class TrainableDropout(nn.Module):
