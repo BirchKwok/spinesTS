5eb282695369addac8fd7741a39132c7bce55568
birchmyname <40603201+BirchKwok@users.noreply.github.com>
2022-04-13 19:27:50 +0800

    Update __init__.py

--- spinesTS/nn/__init__.py
+++ spinesTS/nn/__init__.py
@@ -1,8 +1,9 @@
 __all__ = [
-    'DenseNet', 'WeightedDenseRNN', 'Time2VecNet'
+    'DenseNet', 'WeightedDenseRNN', 'Time2VecNet', 'GAUNet'
 ]
 
 
 from ._dense_net import DenseNet
 from ._weighted_dense_rnn import WeightedDenseRNN
 from ._time2vec_net import Time2VecNet
+from ._gau import GAUNet
