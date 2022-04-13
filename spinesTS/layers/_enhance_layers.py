92adac08c76b24d4c28fcaaa4abbf90b59e4011f
birchmyname <40603201+BirchKwok@users.noreply.github.com>
2022-04-13 19:28:53 +0800

    Update _enhance_layers.py

--- spinesTS/layers/_enhance_layers.py
+++ spinesTS/layers/_enhance_layers.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
 from spinesTS.layers import RecurseResBlock
 
 
@@ -93,3 +94,71 @@ class Time2Vec(nn.Module):
                       self.cosine_w * torch.cos(torch.squeeze(x[i]) + self.P)
 
         return torch.concat((original, x), dim=-1)  # last dimension shape (, 2 * in_features)
+
+
+class GAU(nn.Module):
+    """Base on Transformer Quality in Linear Time(https://arxiv.org/pdf/2202.10447.pdf)
+    
+    """
+    def __init__(
+        self,
+        in_features,
+        query_key_dim = 128,
+        expansion_factor = 2.,
+        add_residual = True,
+        dropout = 0.,
+    ):
+        super().__init__()
+        hidden_dim = int(expansion_factor * in_features)
+
+        self.norm = nn.LayerNorm(in_features)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_hidden = nn.Sequential(
+            nn.Linear(in_features, hidden_dim * 2),
+            nn.SiLU()
+        )
+
+        self.to_qk = nn.Sequential(
+            nn.Linear(in_features, query_key_dim),
+            nn.SiLU()
+        )
+
+        self.gamma = nn.Parameter(torch.ones(2, query_key_dim))
+        self.beta = nn.Parameter(torch.zeros(2, query_key_dim))
+        nn.init.normal_(self.gamma, std=0.02)
+
+
+        self.to_out = nn.Sequential(
+            nn.Linear(hidden_dim, in_features),
+            nn.Dropout(dropout)
+        )
+
+        self.add_residual = add_residual
+
+    def forward(self, x):
+        seq_len = x.shape[-2]
+
+        normed_x = self.norm(x)  # (bs,seq_len,dim)
+        v, gate = self.to_hidden(normed_x).chunk(2, dim = -1)  # (bs,seq_len,seq_len)
+
+        Z = self.to_qk(normed_x)  # (bs,seq_len,query_key_dim)
+
+        QK = torch.einsum('... d, h d -> ... h d', Z, self.gamma) + self.beta
+        q, k = QK.unbind(dim=-2)
+
+        sim = torch.einsum('i d, j d -> i j', q, k) / seq_len
+
+
+        A = F.relu(sim) ** 2
+        A = self.dropout(A)
+
+        V = torch.einsum('i j, j d -> i d', A, v)
+        V = V * gate
+
+        out = self.to_out(V)
+
+        if self.add_residual:
+            out = out + x
+
+        return out
