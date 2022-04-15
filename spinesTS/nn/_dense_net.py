f7351a9bd7a822606d6784d932a7289ade71c2cb
birchmyname <40603201+BirchKwok@users.noreply.github.com>
2022-04-15 15:49:03 +0800

    Update _dense_net.py

--- spinesTS/nn/_dense_net.py
+++ spinesTS/nn/_dense_net.py
@@ -1,7 +1,5 @@
 import torch
 from torch import nn
-
-from spinesTS.utils import seed_everything
 from spinesTS.base import TorchModelMixin
 
 
@@ -78,9 +76,9 @@ class DenseNet(TorchModelMixin):
     """
     spinesTS DenseNet1D pytorch-model
     """
-    def __init__(self, output_nums, kernel_size=3, learning_rate=0.001, random_seed=0):
-        seed_everything(random_seed)
-        self.output_nums = output_nums
+    def __init__(self, in_features, out_features, kernel_size=3, learning_rate=0.001, random_seed=0):
+        super(DenseNet, self).__init__(seed=random_seed)
+        self.out_features = out_features
         self.learning_rate = learning_rate
         self.kernel_size = kernel_size
         self.model, self.loss_fn, self.optimizer = None, None, None
@@ -88,20 +86,34 @@ class DenseNet(TorchModelMixin):
         self.model, self.loss_fn, self.optimizer = self.call()
 
     def call(self):
-        model = dense_net(self.output_nums, kernel_size=self.kernel_size)
+        model = dense_net(self.out_features, kernel_size=self.kernel_size)
         loss_fn = nn.HuberLoss()
         optimizer = torch.optim.AdamW(model.parameters(), lr=self.learning_rate)
         return model, loss_fn, optimizer
 
-    def fit(self, X_train, y_train, epochs=1000, batch_size='auto', eval_set=None,
-            monitor='val_loss', min_delta=0, patience=10,
-            restore_best_weights=True, verbose=True):
+    def fit(
+            self,
+            X_train,
+            y_train,
+            epochs=1000,
+            batch_size='auto',
+            eval_set=None,
+            monitor='val_loss',
+            min_delta=0,
+            patience=10,
+            lr_scheduler='ReduceLROnPlateau',
+            lr_scheduler_patience=10,
+            lr_factor=0.7,
+            restore_best_weights=True,
+            verbose=True,
+            **kwargs
+    ):
+       
         X_train, y_train = torch.Tensor(X_train), torch.Tensor(y_train)
 
-        return self._fit(X_train, y_train, epochs, batch_size, eval_set, loss_type='down', metrics_name='mae',
-                         monitor=monitor, min_delta=min_delta, patience=patience,
-                         restore_best_weights=restore_best_weights, verbose=verbose)
-
-    def predict(self, x):
-        assert self.model is not None, "model not fitted yet."
-        return self._predict(x)
+        return super().fit(X_train, y_train, epochs, batch_size, eval_set, loss_type='down', metrics_name='mae',
+                         monitor=monitor, lr_scheduler=lr_scheduler,
+                         lr_scheduler_patience=lr_scheduler_patience,
+                         lr_factor=lr_factor,
+                         min_delta=min_delta, patience=patience, restore_best_weights=restore_best_weights,
+                         verbose=verbose, **kwargs)
