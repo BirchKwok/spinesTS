912ffd940a2f011cb34be438def0347cf8f52074
birchmyname <40603201+BirchKwok@users.noreply.github.com>
2022-04-19 19:04:19 +0800

    Update _gau.py

--- spinesTS/nn/_gau.py
+++ spinesTS/nn/_gau.py
@@ -1,19 +1,29 @@
 import torch
 from torch import nn
-from spinesTS.layers import GAU, SeriesRecombinationLayer
+from spinesTS.layers import GAU
 from spinesTS.base import TorchModelMixin
 
 
 class GAUBase(nn.Module):
-    def __init__(self, in_features, out_features, flip_features=False):
+    def __init__(self, in_shapes, out_features, flip_features=False, level=2):
         super(GAUBase, self).__init__()
-        self.in_features, self.out_features = in_features, out_features
-        self.gau = GAU(in_features)
+        self.in_shapes_type = type(in_shapes)
+
+        self.in_features, self.out_features = \
+            in_shapes[-1] if self.in_shapes_type == tuple else in_shapes, out_features
+
+        self.gau = nn.ModuleList([
+                GAU(self.in_features) for i in range(level)
+            ])
         if flip_features:
-            self.gau2 = GAU(in_features)
-            ln_layer_in_fea = 2 * in_features
+            self.gau2 = nn.ModuleList([
+                GAU(self.in_features) for i in range(level)
+            ])
+
+            ln_layer_in_fea = \
+                2 * in_shapes[0] * in_shapes[1] if self.in_shapes_type == tuple else 2 * self.in_features
         else:
-            ln_layer_in_fea = in_features
+            ln_layer_in_fea = in_shapes[0] * in_shapes[1] if self.in_shapes_type == tuple else self.in_features
 
         self.flip_features = flip_features
         self.linear = nn.Linear(ln_layer_in_fea, out_features)
@@ -27,34 +37,22 @@ class GAUBase(nn.Module):
         else:
             x = x1
 
-        return self.linear(x)
+        if x.ndim == 2:
+            return self.linear(x)
 
-
-class GAUBase2d(nn.Module):
-    def __init__(self, in_shapes, out_features, mid_dim=128, flip_features=False):
-        super(GAUBase2d, self).__init__()
-        self.in_shapes, self.in_features = in_shapes, mid_dim
-        self.sampling = SeriesRecombinationLayer(self.in_shapes, out_features=self.in_features)
-        self.gau = GAUBase(self.in_features, out_features, flip_features)
-
-    def forward(self, x):
-        x = self.sampling(x)
-        return self.gau(x)
+        return self.linear(x.reshape(-1, x.shape[1] * x.shape[2]))
 
 
 class GAUNet(TorchModelMixin):
-    def __init__(self, in_features, out_features, flip_features=False, learning_rate=0.01, random_seed=42):
+    def __init__(self, in_features, out_features, flip_features=False, level=2, learning_rate=0.01, random_seed=42):
         super(GAUNet, self).__init__(random_seed)
         self.in_features, self.out_features = in_features, out_features
         self.learning_rate = learning_rate
         self.flip_features = flip_features
-        self.model, self.loss_fn, self.optimizer = self.call()
+        self.model, self.loss_fn, self.optimizer = self.call(level)
 
-    def call(self):
-        if isinstance(self.in_features, tuple):
-            model = GAUBase2d(self.in_features, self.out_features, flip_features=self.flip_features)
-        else:
-            model = GAUBase(self.in_features, self.out_features, flip_features=self.flip_features)
+    def call(self, level=2):
+        model = GAUBase(self.in_features, self.out_features, flip_features=self.flip_features, level=level)
         loss_fn = nn.HuberLoss()
         optimizer = torch.optim.AdamW(model.parameters(), lr=self.learning_rate)
         return model, loss_fn, optimizer
