606073cc03f022b28e5f69b2d75f96d83b32790a
birchmyname <40603201+BirchKwok@users.noreply.github.com>
2022-04-19 19:03:10 +0800

    Update _decompose_layers.py

--- spinesTS/layers/_decompose_layers.py
+++ spinesTS/layers/_decompose_layers.py
@@ -27,136 +27,36 @@ class Hierarchical1d(nn.Module):
         return self.odd(x), self.even(x)
 
 
-class HierarchicalEmbedding(nn.Module):
-    def __init__(self, in_features, kernel=5, dropout=0.5, groups=1, hidden_size=1):
-        super(HierarchicalEmbedding, self).__init__()
-        self.kernel_size = kernel
-        self.dilation = 1
-        self.dropout = dropout
-        self.hidden_size = hidden_size
-        self.groups = groups
-        if self.kernel_size % 2 == 0:
-            pad_l = self.dilation * (self.kernel_size - 2) // 2 + 1  # by default: stride==1
-            pad_r = self.dilation * self.kernel_size // 2 + 1  # by default: stride==1
-        else:
-            pad_l = self.dilation * (self.kernel_size - 1) // 2 + 1  # we fix the kernel size of the second layer as 3.
-            pad_r = self.dilation * (self.kernel_size - 1) // 2 + 1
-
-        modules_P = []
-        modules_U = []
-        modules_psi = []
-        modules_phi = []
-        prev_size = 1
-
-        size_hidden = self.hidden_size
-        modules_P += [
-            nn.ReplicationPad1d((pad_l, pad_r)),
-            nn.Conv1d(in_features * prev_size, int(in_features * size_hidden),
-                      kernel_size=self.kernel_size, dilation=self.dilation, stride=1, groups=self.groups),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(self.dropout),
-            nn.Conv1d(int(in_features * size_hidden), in_features,
-                      kernel_size=3, stride=1, groups=self.groups),
-            nn.Tanh()
-        ]
-        modules_U += [
-            nn.ReplicationPad1d((pad_l, pad_r)),
-            nn.Conv1d(in_features * prev_size, int(in_features * size_hidden),
-                      kernel_size=self.kernel_size, dilation=self.dilation, stride=1, groups=self.groups),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(self.dropout),
-            nn.Conv1d(int(in_features * size_hidden), in_features,
-                      kernel_size=3, stride=1, groups=self.groups),
-            nn.Tanh()
-        ]
-
-        modules_phi += [
-            nn.ReplicationPad1d((pad_l, pad_r)),
-            nn.Conv1d(in_features * prev_size, int(in_features * size_hidden),
-                      kernel_size=self.kernel_size, dilation=self.dilation, stride=1, groups=self.groups),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(self.dropout),
-            nn.Conv1d(int(in_features * size_hidden), in_features,
-                      kernel_size=3, stride=1, groups=self.groups),
-            nn.Tanh()
-        ]
-        modules_psi += [
-            nn.ReplicationPad1d((pad_l, pad_r)),
-            nn.Conv1d(in_features * prev_size, int(in_features * size_hidden),
-                      kernel_size=self.kernel_size, dilation=self.dilation, stride=1, groups=self.groups),
-            nn.LeakyReLU(negative_slope=0.01, inplace=True),
-            nn.Dropout(self.dropout),
-            nn.Conv1d(int(in_features * size_hidden), in_features,
-                      kernel_size=3, stride=1, groups=self.groups),
-            nn.Tanh()
-        ]
-        self.phi = nn.Sequential(*modules_phi)
-        self.psi = nn.Sequential(*modules_psi)
-        self.P = nn.Sequential(*modules_P)
-        self.U = nn.Sequential(*modules_U)
-
-    def forward(self, x_even, x_odd):
-
-        x_even = x_even.permute(0, 2, 1)
-        x_odd = x_odd.permute(0, 2, 1)
-
-        d = x_odd - self.P(x_even)
-        c = x_even + self.U(d)
-
-        return c, d
-
-
-class TrainableMovingAverage1d(nn.Module):
+class MoveAvg(nn.Module):
     """Take the moving average of the input sequence.
-        accept a 2-dimensional tensor x, and return a 2-dimensional tensor of the shape:
-            (x.shape[0], x.shape[1] - kernel_size)
+            accept a 2-dimensional tensor x, and return a 2-dimensional tensor of the shape:
+                (x.shape[0], x.shape[1] - kernel_size)
 
     Parameters
     ----------
     kernel_size : int, moving average window size
-    weighted : bool, if true, it is a weighted moving average and the weight is a trainable parameter;
-        otherwise, it is a simple moving average
+    stride : int, the jump-step-length in calculating the moving average
     padding : str, only support to 'same' and 'valid', valid means not to pad, same means padding with
     the nearest valid value to the null value
     Returns
     -------
     torch.Tensor
     """
-
-    def __init__(self, kernel_size, weighted=True, use_bias=True, padding='neighbor'):
+    def __init__(self, kernel_size, stride=1, padding='neighbor'):
         assert padding in ('neighbor', 'valid')
-        super(TrainableMovingAverage1d, self).__init__()
+        super(MoveAvg, self).__init__()
+        self.kernel_size, self.stride = kernel_size, stride
         self.padding = padding
-        self.kernel_size = kernel_size
-        self.use_bias = use_bias
-        if weighted:
-            self.weighted = nn.Parameter(torch.randn(self.kernel_size, 1))
-            if use_bias:
-                self.bias = nn.Parameter(torch.Tensor([0]))
-        else:
-            self.weighted = None
+        # (, , x.shape[-1] - kernel_size + 1 - stride)
+        self.moving_layer = nn.AvgPool1d(kernel_size, stride=stride, padding=0)
 
     def forward(self, x):
-        assert x.ndim == 2, "MovingAverageLayer accept a two dims input."
-        _, cols = x.shape
-
-        res = None
-
-        for i in range(cols - self.kernel_size):
-            if self.weighted is not None:
-                if self.use_bias:
-                    _ = x[:, i: i + self.kernel_size] @ self.weighted + self.bias
-                else:
-                    _ = x[:, i: i + self.kernel_size] @ self.weighted
-            else:
-                _ = torch.mean(x[:, i: i + self.kernel_size], dim=-1, keepdim=True)
-
-            res = _ if res is None else torch.cat((res, _), dim=-1)
-
+        moving_mean = self.moving_layer(x)
+        _ = torch.cat((moving_mean[:, :, -(self.kernel_size - 1 + self.stride - 1):], moving_mean), dim=-1)
+        res = x - _
         if self.padding == 'neighbor':
-            return torch.cat((res[:, :self.kernel_size], res), dim=-1)
-
-        return res
+            return res, _
+        return res, moving_mean
 
 
 class DimensionConv1d(nn.Module):
@@ -175,4 +75,3 @@ class DimensionConv1d(nn.Module):
     def forward(self, x):
         assert x.ndim == 2
         return self.conv1d(x.view(x.shape[0], x.shape[1], 1)).squeeze()
-
