aced2049f306a007ab1cc5009c8585e5a95193f8
BirchKwok <BirchKwok@gmail.com>
2022-04-23 22:52:56 +0800

    add validation codes

--- spinesTS/utils/__init__.py
+++ spinesTS/utils/__init__.py
@@ -1,6 +1,7 @@
 __all__ = [
     'available_if', 'func_has_params',
-    'seed_everything', 'torch_summary', 'one_dim_tensor_del_elements'
+    'seed_everything', 'torch_summary', 'one_dim_tensor_del_elements',
+    'check_is_fitted'
 ]
 
 
@@ -8,3 +9,4 @@ from ._metaestimator import available_if, func_has_params
 from ._set_seed import seed_everything
 from ._torch_summary import torch_summary
 from ._torch_ops import one_dim_tensor_del_elements
+from ._validation import check_is_fitted
