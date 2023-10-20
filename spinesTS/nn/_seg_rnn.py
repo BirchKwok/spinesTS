438d0e28e29055fbdf80c3fc632c6e580bcede8a
birchkwok <birchkwok@gmail.com>
2023-10-20 16:46:41 +0800

    复现SegRNN

--- spinesTS/nn/_seg_rnn.py
+++ spinesTS/nn/_seg_rnn.py
@@ -4,125 +4,147 @@ import torch
 from torch import nn
 
 from spinesTS.base import TorchModelMixin, ForecastingMixin
+from spinesTS.layers import PositionalEncoding3D
+
+
+def get_even_blocks(in_features, window_size):
+    """
+    将输入数据分割成多个块，每个块的大小为window_size
+    :param in_features:
+    :return:
+    """
+
+    blocks = in_features // window_size + int(in_features % window_size != 0)
+
+    while (blocks % 2 != 0 or window_size % 2 != 0) and window_size >= 6:
+        window_size -= 1
+        blocks = in_features // window_size + int(in_features % window_size != 0)
+
+    return blocks, window_size
 
 
 class SegmentationBlock(nn.Module):
-    def __init__(self, in_features, window_size):
+    """将输入数据分割成多个块，每个块的大小为window_size"""
+
+    def __init__(self, in_features, window_size=12):
         super(SegmentationBlock, self).__init__()
 
-        self.encoder = nn.Linear(window_size, window_size)
+        self.blocks, self.window_size = get_even_blocks(in_features, window_size)
+
+        if self.blocks % 2 != 0:
+            self.blocks += 1
+
+        self.encoders = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(self.window_size, self.window_size),
+                nn.ReLU()
+            )
+            for i in range(self.blocks)
+        ])
 
     def forward(self, x):
-        pass
+        assert x.ndim == 2
 
-class EncoderDecoderBlock(nn.Module):
-    def __init__(self, in_features, out_features, num_layers=1, bias=True,
-                 dropout=0., bidirectional=False):
-        super(EncoderDecoderBlock, self).__init__()
-
-        if not bidirectional:
-            decoder_input_features = in_features
+        if self.blocks == 1:
+            xs = [x]
         else:
-            decoder_input_features = in_features * 2
+            xs = list(torch.split(x, self.window_size, dim=1))
+            xs[-1] = torch.concat([xs[-1], torch.zeros(xs[-1].shape[0],
+                                                       self.window_size - xs[-1].shape[1])], dim=1)
 
-        self.bidirectional = bidirectional
+        res = []
+        for i in range(self.blocks):
+            res.append(self.encoders[i](xs[i]))
 
-        self.encoder = nn.LSTM(in_features, in_features, num_layers=num_layers,
-                               bias=bias, dropout=dropout, bidirectional=bidirectional, batch_first=True)
-
-        self.decoder = nn.LSTM(in_features, in_features, num_layers=num_layers,
-                               bias=bias, dropout=dropout, bidirectional=bidirectional, batch_first=True)
-
-        self.linear = nn.Linear(decoder_input_features, out_features)
-        self.selu = nn.SELU()
-        self.dropout = nn.Dropout(dropout)
-        self.attention = nn.Linear(in_features, in_features)
-
-    def forward(self, x):
-        if x.ndim == 2:
-            x = torch.unsqueeze(x, dim=1)
-
-        _, (h, c) = self.encoder(x)
-
-        attention_weights = torch.softmax(self.attention(x), dim=-1)
-        x = x * attention_weights  # Apply attention
-
-        output, (_, _) = self.decoder(x, (h, c))
-
-        return self.selu(self.linear(output.squeeze()))
+        return torch.stack(res, 1)  # [batch_size, blocks, window_size]
 
 
-class Seq2SeqBlock(nn.Module):
-    def __init__(self, in_features, out_features, stack_num=4, num_layers=1, bias=True,
-                 dropout=0., bidirectional=False):
-        super(Seq2SeqBlock, self).__init__()
+class SegRNNBlock(nn.Module):
+    def __init__(self, in_features, out_features, dropout=0.1):
+        super(SegRNNBlock, self).__init__()
 
-        self.in_features, self.out_features = in_features, out_features
+        if in_features < 24:
+            raise ValueError("The number of input features must be equal or greater than 24.")
 
-        self.blocks = nn.ModuleList(
-            [
-                EncoderDecoderBlock(
-                    in_features=in_features,
-                    out_features=in_features, num_layers=num_layers,
-                    bias=bias, dropout=dropout, bidirectional=bidirectional
-                ) for i in range(stack_num)
-            ]
-        )
+        self.splitter = SegmentationBlock(in_features=in_features, window_size=12)
+        window_size = self.splitter.window_size
 
-        self.attention = nn.Linear(in_features, in_features)
+        self.rnn = nn.GRU(window_size, window_size, num_layers=1,
+                          bias=True, bidirectional=False, batch_first=True)
 
-        self.linear_0 = nn.Linear(in_features, 1024)
-        self.selu = nn.SELU()
+        self.block_position_encoder = PositionalEncoding3D(d_model=window_size)
+        self.channel_position_encoder = PositionalEncoding3D(d_model=self.splitter.blocks)
 
-        self.linear_1 = nn.Linear(1024, out_features)
+        out_window_size = out_features // self.splitter.blocks + int(out_features % self.splitter.blocks > 0)
+
+        self.decoders = nn.ModuleList([
+            nn.Sequential(
+                nn.Dropout(dropout),
+                nn.Linear(window_size, 1024),
+                nn.Linear(1024, out_window_size)
+            )
+            for i in range(self.splitter.blocks)
+        ])
+
+        self.out_features = out_features
 
     def forward(self, x):
-        outputs = []
+        x = self.splitter(x)  # [batch_size, blocks, window_size]
 
-        for block in self.blocks:
-            last_output = block(x)
-            last_output = last_output * torch.softmax(self.attention(last_output), dim=-1)
-            outputs.append(last_output)
+        _, h = self.rnn(x)
 
-        output = torch.mean(torch.stack(outputs), dim=0)
+        pos_enc = self.block_position_encoder(x)
 
-        output = self.linear_0(output.squeeze())
-        output = self.selu(output)
+        channel_encs = self.channel_position_encoder(torch.randn(x.shape[0], x.shape[1], self.splitter.blocks))
 
-        return self.linear_1(output)
+        channel_enc = torch.empty(pos_enc.size(0), pos_enc.size(1), pos_enc.size(2), device=x.device)
+
+        # 沿轴复制元素
+        for _ in range(channel_encs.shape[2]):
+            for _2 in range(self.splitter.blocks):
+                if _ * self.splitter.blocks + _2 < pos_enc.size(2):
+                    channel_enc[:, :, _ * self.splitter.blocks + _2] = channel_encs[:, :, _]
+
+        x_enc = x + pos_enc + channel_enc
+
+        output, _ = self.rnn(x_enc, h)
+
+        res = []
+        for i in range(self.splitter.blocks):
+            res.append(self.decoders[i](output[:, i, :]))
+
+        res = torch.concat(res, dim=1)
+
+        return res[:, :self.out_features]
 
 
-class StackingRNN(TorchModelMixin, ForecastingMixin):
+class SegRNN(TorchModelMixin, ForecastingMixin):
+    """长序列预测模型
+
+    References
+    ----------
+    LIN S. SegRNN: Segment Recurrent Neural Network for Long-Term Time Series Forecasting[J]. 2023.
+    arXiv preprint arXiv:2308.11200
+    """
     def __init__(self,
                  in_features: int,
                  out_features: int,
-                 stack_num=4,
-                 num_layers=1,
                  loss_fn='mae',
-                 bias=True,
                  dropout=0.1,
-                 bidirectional=True,
                  learning_rate: float = 0.001,
                  random_seed: int = 42,
                  device='cpu'
                  ) -> None:
-        super(StackingRNN, self).__init__(random_seed, device, loss_fn=loss_fn)
+        super(SegRNN, self).__init__(random_seed, device, loss_fn=loss_fn)
         self.in_features, self.out_features = in_features, out_features
         self.learning_rate = learning_rate
-        self.model, self.loss_fn, self.optimizer = self.call(stack_num=stack_num, num_layers=num_layers, bias=bias,
-                                                             dropout=dropout,
-                                                             bidirectional=bidirectional)
+        self.model, self.loss_fn, self.optimizer = self.call(dropout=dropout)
 
-    def call(self, stack_num, num_layers, bias,
-             dropout,
-             bidirectional) -> tuple:
-        model = Seq2SeqBlock(
+    def call(self, dropout) -> tuple:
+        model = SegRNNBlock(
             in_features=self.in_features,
             out_features=self.out_features,
-            stack_num=stack_num,
-            num_layers=num_layers, bias=bias,
-            dropout=dropout,
-            bidirectional=bidirectional,
+            dropout=dropout
         )
         loss_fn = self.loss_fn
         optimizer = torch.optim.AdamW(model.parameters(), lr=self.learning_rate)
